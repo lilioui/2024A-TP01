077b90eeb532eab08d76d82f71d81bd9df7beb99
“Liligit config --global user.email “lili.no@polymtl.ca <lilmac@MacBook-Pro-de-Lili.local>
2024-09-11 17:44:51 -0400

    modif exo 3

--- ex3.py
+++ ex3.py
@@ -1,5 +1,12 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
 
-speed = ... 
-angle = ...
+import math
+
+speed = float(input("Quelle est la vitesse initiale (en m/s):"))
+angle = float(input("Quel est l'angle du lancé (en degrés):"))
+g = 9.81
+
+distance = ((speed)**2) * (math.sin(math.radians(2*(angle)))) / g
+
+print(round(distance, 2))
