d94d497ef92f428921c1d2e184ddc597bc863e71
Mariya Lahrech <lahrechmariya@gmail.com>
2024-09-11 16:33:25 -0400

    modification

--- ex1.py
+++ ex1.py
@@ -1,5 +1,10 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input("Quel est son nom ? ")
+date = input("Date du record ? ")
+discipline = input("Dans quelle discipline ? ")
+categorie = input("Dans une categorie specifique ? ")
+record= input("Quel est le record ? ")
+
+print("Nouveau Record :\n--------------------","\n",date,"\n",discipline, "-",categorie,"\n",athlete, country, "-", record)
