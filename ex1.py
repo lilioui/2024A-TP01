15734ad833fbb8b83d53e271712e2f57cec3f723
Mariya Lahrech <lahrechmariya@gmail.com>
2024-09-11 16:43:10 -0400

    messss

--- ex1.py
+++ ex1.py
@@ -3,8 +3,8 @@
 country = input("De quelle nationalité est l'athlète ? ")
 athlete = input("Quel est son nom ? ")
 date = input("Date du record ? ")
-discipline = input("Dans quelle discipline ? ")
-categorie = input("Dans une categorie specifique ? ")
+sport = input("Dans quelle discipline ? ")
+category = input("Dans une categorie specifique ? ")
 record= input("Quel est le record ? ")
 
-print("Nouveau Record :\n--------------------","\n",date,"\n",discipline, "-",categorie,"\n",athlete, country, "-", record)
+print("Nouveau Record :\n--------------------","\n",date,"\n",sport, "-",category,"\n",athlete, country, "-", record)
