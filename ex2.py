dea18fd61d4f9a5ad9ff3617ad0165c91d2b6051
Mariyalahrech <lahrechmariya@gmail.com>
2024-09-18 16:28:11 -0400

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -7,4 +7,3 @@ expected_light = int(float(water_quantity)*0.6)
 expected_chlorine = float(water_quantity)*0.1
 
 print(f" Voici les éléments requis pour assainir {water_quantity}L d'eau:\n\t- Filtre(s) : {expected_filter}\n\t- Lampe(s) UV : {expected_light}\n\t- Chlore : {expected_chlorine:.1f}kg")
-
